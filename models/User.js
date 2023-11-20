42184985cf8ff6d65a1ce040dcfffa8038db8b5d
JW0203 <widwoo0203@gmail.com>
2023-11-20 11:00:40 +0900

    feat: add user model

---
+++ models/User.js
@@ -0,0 +1,19 @@
+const sequelize = require('../config/database');
+const {DataTypes} = require("sequelize");
+
+const User = sequelize.define('users',{
+   id :{
+       type: DataTypes.INTEGER,
+       autoIncrement: true,
+       primaryKey: true
+   },
+    email:{
+       type: DataTypes.STRING,
+
+    },
+    password:{
+       type: DataTypes.STRING // INTEGER 이면 빈 공간으로 값을 줄때 알아서 에러가 떠서 STRING 으로 변경해봄
+    }
+})
+
+module.exports = User;
---
+++ models/index.js
@@ -0,0 +1,7 @@
+const User = require('./User.js');
+
+
+
+module.exports ={
+    User
+}
