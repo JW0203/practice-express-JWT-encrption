8fedccbb653e90182bd7dd3c0d4db20c8bb05dd7
JW0203 <widwoo0203@gmail.com>
2023-11-23 01:27:39 +0900

    Add underscore option

--- models/User.js
+++ models/User.js
@@ -14,6 +14,8 @@ const User = sequelize.define('users',{
     password:{
        type: DataTypes.STRING // INTEGER 이면 빈 공간으로 값을 줄때 알아서 에러가 떠서 STRING 으로 변경해봄
     }
+},{
+    underscored:true
 })
 
 module.exports = User;
